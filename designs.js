a5f910024f0b135b46c5614715fd9a5ada16bb35
Ahmed <mohameda7med64@gmail.com>
2018-01-08 00:41:18 +0200

    implemented the makeGrid() function

--- designs.js
+++ designs.js
@@ -4,7 +4,22 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
 // Your code goes here!
+  var rows = document.getElementById('input_height').value; //the height value
+  var columns = document.getElementById('input_width').value; //the width value
+  $('#pixel_canvas').empty() //clears the grid if the user enters new values
+//draws the grid based on user input
+  for (var i = 0; i < rows; i++){
+    $('#pixel_canvas').append('<tr class="row"></tr>');
+    for (var j = 0; j < columns; j++){
+      var rowElement = $('.row').last();
+      $(rowElement).append('<th class="cell"></th>');
+    }
+  }
 
 }
+
+$('#sizePicker').submit(function(ev){
+  ev.preventDefault();
+  makeGrid();
+});
