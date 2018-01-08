81cdec0c6a099dea645c2dbed92091cae3a882f3
Ahmed <mohameda7med64@gmail.com>
2018-01-08 02:25:19 +0200

    implemented the draw functionality

--- designs.js
+++ designs.js
@@ -1,25 +1,43 @@
 // Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+var mouseDown = false;
 
+// draws the grid after user submits height and width
 function makeGrid() {
-// Your code goes here!
   var rows = document.getElementById('input_height').value; //the height value
   var columns = document.getElementById('input_width').value; //the width value
   $('#pixel_canvas').empty() //clears the grid if the user enters new values
-//draws the grid based on user input
-  for (var i = 0; i < rows; i++){
-    $('#pixel_canvas').append('<tr class="row"></tr>');
-    for (var j = 0; j < columns; j++){
+
+  //draws the grid based on user input
+  for (var row = 0; row < rows; row++){
+    $('#pixel_canvas').append('<tr class="row"></tr>'); //adds new row
+    for (var column = 0; column < columns; column++){
       var rowElement = $('.row').last();
-      $(rowElement).append('<td class="cell">&nbsp;</td>');
+      $(rowElement).append('<td class="cell"></td>'); //adds new cells
     }
   }
-
 }
 
+
 $('#sizePicker').submit(function(ev){
-  ev.preventDefault();
+  ev.preventDefault(); //prevents the submit button from submitting data
   makeGrid();
+});
+
+$(document).mousedown(function(){
+  mouseDown = true;
+}).mouseup(function(){
+  mouseDown = false;
+});
+
+$('#pixel_canvas').on('mouseout', 'td', function(){
+  if(mouseDown){
+    var color = document.getElementById('colorPicker').value;
+    $(this).css('background-color', color);
+  }
+});
+$('#pixel_canvas').on('click', 'td', function(){
+  var color = document.getElementById('colorPicker').value;
+  $(this).css('background-color', color);
 });
